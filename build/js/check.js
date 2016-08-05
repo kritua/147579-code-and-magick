c19d7608416ba752a1fe7e201dfdf6868805bed3
kritua <kritua2@gmail.com>
2016-08-05 14:08:11 +0300

    Changed code
    Used "===" instead of "=="

--- build/js/check.js
+++ build/js/check.js
@@ -1,13 +1,13 @@
 function getMessage (a, b) {
-    if (typeof a == 'boolean') {
-        if (a == true) {
+    if (typeof a === 'boolean') {
+        if (a === true) {
             return "Я попал в " + b;
         }
         else {
             return "Я никуда не попал";
         }
     }
-    if (typeof a == 'number') {
+    if (typeof a === 'number') {
         return  "Я прыгнул на " + a * 100 + " сантиметров";
     }
 
