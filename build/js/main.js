679abe8e30448a40561151f2262a96009703926b
Kri Tua <kritua2@gmail.com>
2016-08-13 03:16:16 +0300

    Module3 Task2 First try
    Stars rating isn't working

--- build/js/main.js
+++ build/js/main.js
@@ -115,24 +115,54 @@
 	var name = document.querySelector('#review-name');
 	var text = document.querySelector('#review-text');
 	var stars = document.getElementsByName('review-mark');
-	var value = document.getElementsByName('review-mark').value;
 	var submitButton = document.querySelector('.review-submit');
+	var nameLabel = document.querySelector('.review-fields-label');
+	var textLabel = document.querySelector('.review-fields-text');
+	var reviewField = document.querySelector('.review-fields');
+	var reviewForm = document.querySelector('.review-form');
+	
+	stars[2].defaultChecked = false;
 	
 	
-	name.required = true;
 	
-	if (name.length < 0 || text.value == '') {
+	for (var i = 0; i < stars.length; i++) {
+	  if (stars.checked === true && stars[i].value <= 2) {
+	    text.required = true;
+	  }
+	}
+	
+	name.required = 'true';
+	name.min = 3;
+	
+	if (name.value <= 0 || text.value == '') {
 	  submitButton.disabled = true;
 	}
 	
-	name.onchange = function() {
-	  submitButton.disabled = false;
+	reviewForm.oninput = function() {
+	  if (name.value != 0) {
+	    nameLabel.style.visibility = 'hidden';
+	    submitButton.disabled = false;
+	  }
+	  else {
+	    nameLabel.style.visibility = 'visible';
+	    submitButton.disabled = true;
+	  }
+	
+	  if (text.value != '') {
+	    textLabel.style.visibility = 'hidden';
+	  }
+	  else {
+	    textLabel.style.visibility = 'visible';
+	  }
+	  
+	  if (text.value != '' && name.value != 0) {
+	    reviewField.style.visibility = 'hidden';
+	  }
+	  else {
+	    reviewField.style.visibility = 'visible';
+	  }
 	};
 	
-	
-	  if (value < 3) {
-	    text.required = true;
-	  }
 	
 	
 
