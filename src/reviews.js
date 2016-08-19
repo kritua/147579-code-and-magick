363746ca2b35b7a82c577e65c53e9c5bf382fd15
kritua <kritua2@gmail.com>
2016-08-19 21:07:39 +0300

    Changes in cookies saving function

--- src/reviews.js
+++ src/reviews.js
@@ -1,3 +1,35 @@
+'use strict';
+
+window.CallBackRegistry = {};
+
 /**
- * Created by ROFL on 18.08.2016.
+ * JSONP запрос
+ * @param {string} link адрес
+ * @param {function} callback callback-функция
  */
+
+function reviewsAdd(link, callback) {
+  var callbackVar = 'callback' + String(Math.random()).slice(-6);
+  var script = document.createElement('script');
+
+  link += link + 'callback=reviews.' + callbackVar;
+  window.CallBackRegistry[callbackVar] = function(data) {
+    callback(data);
+  };
+
+  script.onload = script.onerror = function() {
+    delete window.CallBackRegistry[callbackVar];
+    document.body.removeChild(script);
+  };
+
+  script.src = link;
+  document.body.appendChild(script);
+}
+
+window.reviews = null;
+
+function reviewData(data) {
+  window.reviews = data;
+}
+
+reviewsAdd('http://localhost:1506/api/reviews', reviewData);
