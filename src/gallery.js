6fb44eea72c4920e9f29a36c659466913c0e037a
kritua <kritua2@gmail.com>
2016-08-29 21:10:21 +0300

    First commit
    Totally useless

--- src/gallery.js
+++ src/gallery.js
@@ -1,36 +1,54 @@
-'use strict';
-
 var Gallery = function(pictures) {
-
-	var self = this;
-
-	this.pictures = pictures;
-	this.activePicture = 0;
 	this.picturesAll = document.querySelectorAll('.photogallery-image');
 	this.overlayGallery = document.querySelector('.overlay-gallery');
 	this.leftControl = document.querySelector('.overlay-gallery-control-left');
 	this.rightControl = document.querySelector('.overlay-gallery-control-right');
 	this.closeGallery = document.querySelector('.overlay-gallery-close');
 	this.currentSlide = document.querySelector('.preview-number-current');
-	this.totalSlides = document.querySelector('.preview-number-total');
 
-	this.closeGallery.onclick = function() {
-		self.overlayGallery.classList.add('invisible');
-	}
-
-
+	this.pictures = pictures;
+	this.activePicture = 0;
 };
 
-
-
-
-Gallery.prototype.show = function() {
-
-	overlayGallery.classList.remove('invisible');
-};
-
-
-
-var galleryShow = new Gallery(pictures);
-
-module.exports = Gallery;
+/*
+ Открываем галерею
+ */
+Gallery.prototype.show = function(activePicture) {
+	this.picturesAll.addEventListener('click', )
+// Вешаем обработчики событий. Лучше это сделать через addEventListener
+// прочитай про метод .bind
+// Выглядит примерно так addEventListener('click', this.next.bind(this)), или использовать var self = this  и addEventListener('click', self.next)
+// this.overlayGallery убираем класс invisible
+// вызываем setActivePicture для выбора и показа картинки
+}
+/*
+ Закрываем галерею
+ */
+Gallery.prototype.hide = function() {
+// ставим класс invisible
+// убираем обработчики this.removeEventListener
+}
+/*
+ Показ изображения
+ */
+Gallery.prototype.setActivePicture = function(activePicture) {
+// создаем картинку
+// сохраняем активную картинку
+// выбираем картинку из нашего массива адресов
+// Смотрим если уже картинка до этого была добавлена то ее заменяем.
+// Если нет то добавляем
+}
+/*
+ Следующая картинка
+ */
+Gallery.prototype.nextPicture = function() {
+// проверяем если activePicture+1 меньше длины массива
+// То вызываем setActivePicture(нужный номер) 
+}
+/*
+ Предыдущая картинка
+ */
+Gallery.prototype.prevPicture = function() {
+// проверяем если this.activePicture - 1 >= 0
+// Вызываем setActivePicture
+}
