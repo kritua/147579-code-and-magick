f7866c847cb85bedc2ad20bfa667610a829c5240
Kri Tua <kritua2@gmail.com>
2016-08-31 02:39:57 +0300

    Module6 Task1 done

--- src/gallery.js
+++ src/gallery.js
@@ -1,82 +1,76 @@
+'use strict';
+
 var Gallery = function(pictures) {
-	this.overlayGallery = document.querySelector('.overlay-gallery');
-	this.leftControl = document.querySelector('.overlay-gallery-control-left');
-	this.rightControl = document.querySelector('.overlay-gallery-control-right');
-	this.closeGallery = document.querySelector('.overlay-gallery-close');
-	this.currentSlide = document.querySelector('.preview-number-current');
-	this.previewSlide = document.querySelector('.overlay-gallery-preview');
+  this.overlayGallery = document.querySelector('.overlay-gallery');
+  this.leftControl = document.querySelector('.overlay-gallery-control-left');
+  this.rightControl = document.querySelector('.overlay-gallery-control-right');
+  this.closeGallery = document.querySelector('.overlay-gallery-close');
+  this.currentSlide = document.querySelector('.preview-number-current');
+  this.previewSlide = document.querySelector('.overlay-gallery-preview');
 
-	this.picturesLink = document.querySelectorAll('.photogallery-image');
-	this.picturesAll = document.querySelectorAll('.photogallery-image img');
+  this.nextSlide = this.nextPicture.bind(this);
+  this.prevSlide = this.prevPicture.bind(this);
+  this.hideGallery = this.hide.bind(this);
 
-
-
-	this.nextSlide = this.nextPicture.bind(this);
-	this.prevSlide = this.prevPicture.bind(this);
-	this.hideGallery = this.hide.bind(this);
-
-	this.pictures = pictures;
-	this.activePicture = 0;
+  this.pictures = pictures;
+  this.activePicture = 0;
+  this.currentPicture = null;
 };
 
 /*
  Открываем галерею
  */
 Gallery.prototype.show = function(activePicture) {
-	this.closeGallery.addEventListener('click', this.hideGallery);
-	this.rightControl.addEventListener('click', this.nextSlide);
-	this.leftControl.addEventListener('click', this.prevSlide);
+  this.closeGallery.addEventListener('click', this.hideGallery);
+  this.rightControl.addEventListener('click', this.nextSlide);
+  this.leftControl.addEventListener('click', this.prevSlide);
 
-	this.overlayGallery.classList.remove('invisible');
+  this.overlayGallery.classList.remove('invisible');
 
-	this.setActivePicture(activePicture);
-// Вешаем обработчики событий. Лучше это сделать через addEventListener
-// прочитай про метод .bind
-// Выглядит примерно так addEventListener('click', this.next.bind(this)), или использовать var self = this  и addEventListener('click', self.next)
-// this.overlayGallery убираем класс invisible
-// вызываем setActivePicture для выбора и показа картинки
+  this.setActivePicture(activePicture);
 };
 /*
  Закрываем галерею
  */
 Gallery.prototype.hide = function() {
-	this.overlayGallery.classList.add('invisible');
-	this.closeGallery.removeEventListener('click', this.hideGallery);
-	this.rightControl.removeEventListener('click', this.nextSlide);
-	this.leftControl.removeEventListener('click', this.prevSlide);
-// ставим класс invisible
-// убираем обработчики this.removeEventListener
+  this.overlayGallery.classList.add('invisible');
+  this.closeGallery.removeEventListener('click', this.hideGallery);
+  this.rightControl.removeEventListener('click', this.nextSlide);
+  this.leftControl.removeEventListener('click', this.prevSlide);
 };
 /*
  Показ изображения
  */
 Gallery.prototype.setActivePicture = function(activePicture) {
-	var image = new Image();
-	this.activePicture = activePicture;
+  var image = new Image();
+  this.activePicture = activePicture;
 
-	if (this.currentSlide) {
-		this.previewSlide.replaceChild(image, this.currentSlide);
-	} else {
-		this.previewSlide.appendChild(image);
-	}
+  image.src = this.pictures[activePicture];
 
-// создаем картинку
-// сохраняем активную картинку
-// выбираем картинку из нашего массива адресов
-// Смотрим если уже картинка до этого была добавлена то ее заменяем.
-// Если нет то добавляем
-}
+  if (this.currentPicture) {
+    this.previewSlide.replaceChild(image, this.currentPicture);
+  } else {
+    this.previewSlide.appendChild(image);
+  }
+
+  this.currentSlide.innerHTML = this.activePicture + 1;
+  this.currentPicture = image;
+};
 /*
  Следующая картинка
  */
 Gallery.prototype.nextPicture = function() {
-// проверяем если activePicture+1 меньше длины массива
-// То вызываем setActivePicture(нужный номер) 
-}
+  if (this.activePicture + 1 < this.pictures.length) {
+    this.setActivePicture(this.activePicture + 1);
+  }
+};
 /*
  Предыдущая картинка
  */
 Gallery.prototype.prevPicture = function() {
-// проверяем если this.activePicture - 1 >= 0
-// Вызываем setActivePicture
-}
+  if (this.activePicture - 1 >= this.pictures.length) {
+    this.setActivePicture(this.activePicture - 1);
+  }
+};
+
+module.exports = Gallery;
--- src/main.js
+++ src/main.js
@@ -8,26 +8,44 @@ var reviewData = require('./reviews');
 var reviewsLoad = require('./load');
 var apiURL = 'http://localhost:1506/api/reviews';
 
+var picturesLink = document.querySelectorAll('.photogallery-image');
+var picturesAll = document.querySelectorAll('.photogallery-image img');
 
 var game = new Game(document.querySelector('.demo'));
 var formOpenButton = document.querySelector('.reviews-controls-new');
 
 /** @param {MouseEvent} evt */
-formOpenButton.onclick = function (evt) {
-	evt.preventDefault();
-	form.open(function () {
-		game.setGameStatus(Game.Verdict.PAUSE);
-		game.setDeactivated(true);
-	});
+formOpenButton.onclick = function(evt) {
+  evt.preventDefault();
+  form.open(function() {
+    game.setGameStatus(Game.Verdict.PAUSE);
+    game.setDeactivated(true);
+  });
 };
 
-form.onClose = function () {
-	game.setDeactivated(false);
+form.onClose = function() {
+  game.setDeactivated(false);
 };
 
 game.initializeLevelAndStart();
 game.setGameStatus(Game.Verdict.INTRO);
 
 reviewsLoad(apiURL, reviewData);
+
+
+var pictures = [];
+
+var picturesArray = Array.prototype.slice.call(picturesAll);
+for (var y = 0; y < picturesArray.length; y++) {
+  pictures[y] = picturesArray[y].src;
+}
+
+var gallery = new Gallery(pictures);
+
+Array.prototype.forEach.call(picturesLink, function(item, i) {
+  item.addEventListener('click', gallery.show.bind(gallery, i));
+});
+
+
 
 
